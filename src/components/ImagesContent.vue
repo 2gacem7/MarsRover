b9e184fbfe365d4f7d6682b70ec7aa15d6cf2c77
2gacem7 <gacemcode@gmail.com>
2023-07-06 18:37:16 +0200

    [API] - Adding the infinite scroll

--- src/components/ImagesContent.vue
+++ src/components/ImagesContent.vue
@@ -11,6 +11,7 @@
 <script>
 import ImageCard from "./ImageCard.vue";
 import { marsRoverService } from "@/services";
+
 export default {
   name: "ImagesContent",
   components: {
@@ -21,35 +22,77 @@ export default {
       nameOfRover: "curiosity",
       pageNumber: 1,
       items: [],
+      curiosityData: [],
+      opportunityData: [],
+      spiritData: [],
+      isFetching: false,
     };
   },
   mounted() {
     this.fetchData();
+    window.addEventListener("scroll", this.handleScroll);
   },
-
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
   methods: {
-    async fetchData() {
+    async fetchData(rover) {
       try {
+        this.isFetching = true;
         const response = await marsRoverService.getMarsRover(
           this.nameOfRover,
           this.pageNumber
         );
-        this.items = response.data.photos;
+        const newData = response.data.photos;
+        if (this.nameOfRover === "curiosity") {
+          // Merging the new data with the old data in case of infinite scroll
+          this.curiosityData = [...this.curiosityData, ...newData];
+          this.items = this.curiosityData;
+        } else if (rover === "opportunity") {
+          // Merging the new data with the old data in case of infinite scroll
+          this.opportunityData = [...this.opportunityData, ...newData];
+          this.items = this.opportunityData;
+        } else if (rover === "spirit") {
+          // Merging the new data with the old data in case of infinite scroll
+          this.spiritData = [...this.spiritData, ...newData];
+          this.items = this.spiritData;
+        }
+        this.pageNumber++;
+        this.isFetching = false;
       } catch (error) {
         console.error("An error occurred:", error);
+        this.isFetching = false;
       }
     },
     selectRover(rover) {
-      console.log(rover);
       this.nameOfRover = rover;
-      console.log(rover);
-      this.fetchData();
+      if (rover === "opportunity" && this.opportunityData.length > 0) {
+        return (this.items = this.opportunityData);
+      }
+      if (rover === "curiosity" && this.curiosityData.length > 0) {
+        return (this.items = this.curiosityData);
+      }
+      if (rover === "spirit" && this.spiritData.length > 0) {
+        return (this.items = this.spiritData);
+      }
+      // Reset pageNumber
+      this.pageNumber = 1;
+      this.fetchData(rover);
+    },
+    handleScroll() {
+      if (
+        window.innerHeight + window.pageYOffset >=
+        document.documentElement.offsetHeight - 500
+      ) {
+        if (!this.isFetching) {
+          this.fetchData();
+        }
+      }
     },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .container {
   display: flex;
